11efaa2898751694c20b8f38a1549f773ee1bf2b
Pedro24M <exacadrillex@gmail.com>
2024-05-24 15:19:52 -0300

    Criação do modelo da agenda e ajuste do .env #20

---
+++ api/models/model_agenda.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const agendaSchema = new mongoose.Schema({
+    nome: {type: String, trim: true, uppercase: true, required: true},
+    anfitriao: {type: String, trim: true, uppercase: true, required: true},
+    participantes: {type: Number, required: true},
+    data: {type: String, trim: true, uppercase: true, required: true},
+});
+
+module.exports = mongoose.model("agenda", agendaSchema)
