417cba5710de8c33ca5c37d08bc186a278bcd3b0
Pedro24M <exacadrillex@gmail.com>
2024-05-27 09:55:38 -0300

    Aplicando a logica do controller e criando o router #18 #19

--- api/controllers/controller_agenda.js
+++ api/controllers/controller_agenda.js
@@ -1,5 +1,56 @@
 const mongoose = require('mongoose')
 const Agenda = require("../models/model_agenda")
 
+async function validarDados(req, res, next) {
+    const agenda = new Agenda(req.body);
+    try {
+        await agenda.validate();
+        next();
+    } catch (err) {
+        res.status(422).json({ msg: "Dados da agenda invalidos"})
+    }
+}
 
-module.exports = {};
+async function criar(req, res) {
+    const agenda = await Agenda.create(req.body);
+    res.status(201).json(agenda)
+}
+
+async function listarTodos(req, res) {
+    const agendas = await Agenda.find({});
+    res.json(agendas);
+};
+
+async function buscarPeloId(req, res, next) {
+    try {
+        const id = new mongoose.Types.ObjectId(req.params.id);
+        const agenda = await Agenda.findOne({ _id: id });
+        if (agenda) {
+            next();
+        } else {
+            res.status(404).json({msg: "agenda não encontrada"})
+        }
+    } catch (err) {
+        res.status(400).json({msg: "Id invalido"});
+    }
+};
+
+async function obter(req, res, next) {
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    const agenda = await Agenda.findOne({ _id: id });
+    res.json(agenda);
+};
+
+async function atualizar(req, res) {
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    const agenda = await Agenda.findOneAndUpdate({ _id: id }, req.body);
+    res.json({});
+};
+
+async function remover(req,res) {
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    const agenda = await Agenda.findOneAndDelete({ _id: id });
+    res.status(204).end()
+}
+
+module.exports = {validarDados, criar, listarTodos, buscarPeloId, obter, atualizar, remover}
--- api/routes/router_agenda.js
+++ api/routes/router_agenda.js
@@ -1,56 +1,17 @@
 const express = require("express");
-const mongoose = require("mongoose");
 
-const controller = require("../controllers/controller_agenda")
-
-const Agenda = require("../models/model_agenda")
+const controllerAgenda = require("../controllers/controller_agenda")
 
 const router = express.Router();
 
-router.post("/", 
-    function criar(req, res) {
-        res.status(201).json({});
-    },
-    function validarDados1(req, res) {
-        res.status(422).json({ msg: "Dados do produto invalidos"}); 
-    },
-);
+router.post("/", controllerAgenda.validarDados, controllerAgenda.criar  );
 
-router.get("/", 
-    function buscar(req, res) {
-        res.status(200).json({})
-} 
-);
+router.get("/", controllerAgenda.listarTodos);
 
-router.get("/:id", 
-    function buscarPeloId(req, res) {
-        res.status(200).json({})
-    },
-    function buscarPeloId1(req, res) {
-        res.status(404).json({})
-    },
+router.get("/:id", controllerAgenda.buscarPeloId, controllerAgenda.obter)
 
-)
+router.put("/:id", controllerAgenda.buscarPeloId, controllerAgenda.validarDados, controllerAgenda.atualizar);
 
-router.put("/:id", 
-    function alterar(req, res) {
-        res.status(200).json({});
-    },
-    function alterar1(req, res) {
-        res.status(404).json({});
-    },
-    function alterar2(req, res) {
-        res.status(422).json({});
-    }
-);
-
-router.delete("/:id", 
-    function deletar(req, res) {
-        res.status(204).json({});
-    },
-    function deletar1(req, res) {
-        res.status(404).json({});
-    }
-);
+router.delete("/:id", controllerAgenda.buscarPeloId, controllerAgenda.remover);
 
 module.exports = router;
