388bcd4ee5e63f077aa9ed18b20b075bd854eca5
rayanemariade <141177599+rayanemariade@users.noreply.github.com>
2024-06-03 19:39:58 -0300

    Arquivo Model Amigos

---
+++ api/models/Amigos.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const AmigosSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
+
+module.exports = mongoose.model('Amigos', AmigosSchemaSchema);
