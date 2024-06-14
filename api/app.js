84ebb3745cb6408cf2b6181c7c4bb0cd65ef1a12
gabrielbsb18 <gabrielbsb18@outlook.com>
2024-06-14 10:27:49 -0300

    Adiciona rotas de usuário. Closes #2

--- api/app.js
+++ api/app.js
@@ -1,12 +1,32 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const app = express();
+
+// Conexão ao MongoDB
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Conectado ao MongoDB');
+}).catch((err) => {
+    console.error('Erro ao conectar ao MongoDB', err);
+});
+
+const routerApidocs = require('./routes/router_apidocs');
+const userRouter = require('./routes/router_user');
+const authRouter = require('./routes/auth');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/api-docs', routerApidocs);
+app.use('/user', userRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
