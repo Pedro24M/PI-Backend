c77b900e254e5cddb5205b3e37ac8749df83d436
Pedro24M <exacadrillex@gmail.com>
2024-06-17 09:45:13 -0300

    Zerando a branch develop

--- api/controllers/serviceController.js
+++
@@ -1,7 +0,0 @@
-const {Servico: ServiceModel} = require("../models/Servico")
-
-const servicoController = {
-
-}
-
-module.exports = servicoController
--- api/routes/router.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-
-// Servico Rotas
-const servicoRouter = require("./servico")
-
-router.use("/", servicoRouter)
-
-module.exports = router;
